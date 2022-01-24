33b6f5ea7a89d6775e9ccd93e80707df2b2fdf85
Gahan Rakholia <gahan94rakh@gmail.com>
2022-01-25 01:56:23 +0530

    test: move from pb_dynamic_compiler to code_load

--- test/grpc_mock/pb_dynamic_compiler_test.exs
+++ test/grpc_mock/pb_dynamic_compiler_test.exs
@@ -1,67 +1,29 @@
 defmodule GrpcMock.PbDynamicCompilerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest GrpcMock.PbDynamicCompiler
   alias GrpcMock.PbDynamicCompiler
+  import GrpcMock.PbDynamicCompiler.ModuleStore
 
   setup do
-    start_supervised(PbDynamicCompiler)
-    import_path = Path.join([__DIR__, "..", "support", "fixtures"])
+    Mnesiac.init_mnesia(node())
 
-    on_exit(fn -> GenServer.stop(PbDynamicCompiler) end)
+    on_exit(fn ->
+      :mnesia.transaction(fn -> :mnesia.clear_table(:module) end)
+    end)
 
-    %{import_path: import_path}
-  end
-
-  describe "codegen/2" do
-    test "generates elixir file for provided proto file", %{import_path: import_path} do
-      proto_file = "helloworld.proto"
-      PbDynamicCompiler.codegen(import_path, proto_file)
-      result = :sys.get_state(PbDynamicCompiler)
-
-      assert result ==
-               MapSet.new([
-                 GprcMock.Protos.Helloworld.Greeter.Service,
-                 GprcMock.Protos.Helloworld.Greeter.Stub,
-                 GprcMock.Protos.Helloworld.HelloReply,
-                 GprcMock.Protos.Helloworld.HelloRequest
-               ])
-    end
-
-    test "compiled_modules state remains unchanged when import_path is incorrect" do
-      proto_file = "helloworld.proto"
-      PbDynamicCompiler.codegen("/foo/bar", proto_file)
-      result = :sys.get_state(PbDynamicCompiler)
-
-      assert result == MapSet.new()
-    end
-
-    test "compiled_modules state remains unchanged when proto file is invalid",
-         %{import_path: import_path} do
-      proto_file = "foo.bar"
-      PbDynamicCompiler.codegen(import_path, proto_file)
-      result = :sys.get_state(PbDynamicCompiler)
-
-      assert result == MapSet.new()
-    end
+    :ok
   end
 
   describe "available_modules/0" do
-    test "returns all the loaded modules", %{import_path: import_path} do
-      proto_file = "helloworld.proto"
-      PbDynamicCompiler.codegen(import_path, proto_file)
-
-      # wait till cast call completes
-      :sys.get_state(PbDynamicCompiler)
+    test "returns all the key present in module table" do
+      :mnesia.transaction(fn ->
+        :mnesia.write(module(id: :foo, name: :foo))
+        :mnesia.write(module(id: :bar, name: :bar))
+      end)
 
       result = PbDynamicCompiler.available_modules()
 
-      assert result ==
-               MapSet.new([
-                 GprcMock.Protos.Helloworld.Greeter.Service,
-                 GprcMock.Protos.Helloworld.Greeter.Stub,
-                 GprcMock.Protos.Helloworld.HelloReply,
-                 GprcMock.Protos.Helloworld.HelloRequest
-               ])
+      assert result == [:foo, :bar]
     end
   end
 end
